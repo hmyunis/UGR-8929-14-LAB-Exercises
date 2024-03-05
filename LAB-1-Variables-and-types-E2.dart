86ecef8a695c2cbfc0b181f8eb56ccf4a1ce2bec
Hamdi M <134316722+hmyunis@users.noreply.github.com>
2024-03-05 15:39:16 +0300

    LAB-1-Variables-and-types Exercise 2 solution.

---
+++ LAB-1-Variables-and-types-E2.dart
@@ -0,0 +1,10 @@
+/* 
+Exercise 2: Create a variable str of type String and assign it a value. Then, print the length of the string.
+*/
+
+void main() {
+  String str = "Hi! I am Hamdi.";
+  int length = str.length;
+
+  print(length);
+}
