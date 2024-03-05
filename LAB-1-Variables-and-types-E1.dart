b7870b318a9c1ccb66647a6740053a736d2d4fde
Hamdi M <134316722+hmyunis@users.noreply.github.com>
2024-03-05 15:39:01 +0300

    LAB-1-Variables-and-types Exercise 1 solution.

---
+++ LAB-1-Variables-and-types-E1.dart
@@ -0,0 +1,13 @@
+/* 
+Exercise 1: Declare three variables, a, b, and c, assign them integer values and print their sum.
+*/
+
+void main() {
+  int a = 1;
+  int b = 2;
+  int c = 3;
+
+  int sum = a + b + c;
+
+  print(sum);
+}
