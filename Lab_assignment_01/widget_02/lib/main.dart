921d3790ce53541f637c39c382b0cd1b5c59d9a3
Hamdi M <134316722+hmyunis@users.noreply.github.com>
2024-04-02 16:59:06 +0300

    minor fixes.

--- Lab_assignment_01/widget_02/lib/main.dart
+++ Lab_assignment_01/widget_02/lib/main.dart
@@ -11,6 +11,88 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        bottomSheet: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(15))),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Subtotal",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Text(
+                      "\$60.98",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "TAX (10.0%)",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Text(
+                      "\$6.10",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(80)),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(""),
+                          Text(
+                            "Checkout",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            "\$67.08",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      )),
+                )
+              ],
+            ),
+          ),
+        ),
         appBar: AppBar(
           title: Text(
             "Cart",
@@ -36,7 +118,7 @@ class Home extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              subtitle: Text("Verify"),
+              subtitle: Text("Verify your quantity and click checkout"),
               leading: Icon(
                 Icons.shopping_cart,
               ),
@@ -46,33 +128,35 @@ class Home extends StatelessWidget {
                 children: [
                   ListTile(
                     title: Text('Calas'),
-                    subtitle: Text('This is subtitle 1'),
+                    subtitle: Text('\$15.00'),
                     leading: Container(
                       child: Image.asset('assets/pasta.jpg'),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10)),
                     ),
-                    trailing: Icon(Icons.arrow_right),
-                  ),
-                  ListTile(
-                    title: Text('Calas'),
-                    subtitle: Text('This is subtitle 1'),
-                    leading: Container(
-                      child: Image.asset('assets/pasta.jpg'),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10)),
+                    trailing: Column(
+                      children: [
+                        Icon(Icons.add_circle_outline),
+                        Text("1.0"),
+                        Icon(Icons.remove_circle_outline),
+                      ],
                     ),
-                    trailing: Icon(Icons.arrow_right),
                   ),
                   ListTile(
                     title: Text('Salad'),
-                    subtitle: Text('This is subtitle 1'),
+                    subtitle: Text('\$22.99'),
                     leading: Container(
                       child: Image.asset('assets/salad.jpg'),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10)),
                     ),
-                    trailing: Icon(Icons.arrow_right),
+                    trailing: Column(
+                      children: [
+                        Icon(Icons.add_circle_outline),
+                        Text("1.0"),
+                        Icon(Icons.remove_circle_outline),
+                      ],
+                    ),
                   ),
                 ],
               ),
