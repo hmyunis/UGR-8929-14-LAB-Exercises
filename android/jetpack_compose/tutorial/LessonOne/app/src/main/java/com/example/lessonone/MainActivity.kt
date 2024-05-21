54e244f5cf43a68c24501b2bed915d7a85363890
hmyunis <hamdim801@gmail.com>
2024-05-21 08:40:55 +0300

    Online Compose Tutorial Lessons.

---
+++ android/jetpack_compose/tutorial/LessonOne/app/src/main/java/com/example/lessonone/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lessonone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(
+        text = "Hello $name!",
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
---
+++ android/jetpack_compose/tutorial/LessonTwo/app/src/main/java/com/example/lessontwo/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.lessontwo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "I am learning layouts"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MessageCardPreview() {
+    MessageCard(Message("Android", "I am learning layouts"))
+}
